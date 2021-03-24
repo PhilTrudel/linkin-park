f2eda45e0902060688d48f3e85d38662e6fb33ae
PhilTrudel <61752595+PhilTrudel@users.noreply.github.com>
2021-03-24 16:16:32 -0400

    Début animation page accueil

--- scripts/accueil.js
+++ scripts/accueil.js
@@ -6,3 +6,31 @@ const swiper = new Swiper('.swiper-container', {
       clickable: true,
     },
   });
+
+  gsap.registerPlugin(ScrollTrigger); 
+
+  let sections = document.querySelectorAll(".section");
+
+  sections.forEach(section => {
+    let title = section.querySelectorAll("#title");
+    let card = section.querySelectorAll(".card");
+    let carroussel = section.querySelectorAll(".swiper-container");
+    let video = section.querySelectorAll("iframe");
+
+    gsap.timeline({
+      scrollTrigger: {
+        start: "top 60%",
+        trigger: section,
+        toggleActions: "play none none reverse",
+      }
+    })
+
+    .from(title, 
+      {x: -25, opacity: 0})
+    .from(card,
+      {y: 25, opacity: 0}, '-= 0.1')
+    .from(carroussel,
+      {y: 25, opacity: 0}, '-= 0.3')
+    .from(video,
+      {y: 25, opacity: 0}, '-= 0.3')
+  });
