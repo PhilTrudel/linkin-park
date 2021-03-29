9cfd8c1c9bf586c051155322ed3b4d7eddd32927
Gabeauch <gab.beauchemin27@gmail.com>
2021-03-28 23:29:17 -0400

    animations sections discographie

--- scripts/discographie.js
+++ scripts/discographie.js
@@ -6,3 +6,31 @@ const swiper = new Swiper('.swiper-container', {
       disableOnInteraction: false,
     },
   });
+
+  gsap.registerPlugin(ScrollTrigger);
+
+  let zones = document.querySelectorAll(".section");
+
+  zones.forEach(section => {
+    let title = section.querySelectorAll("#title");
+    let list = section.querySelectorAll(".list");
+    let image = section.querySelectorAll(".image");
+    let btn = section.querySelectorAll(".btn");
+
+    gsap.timeline({
+      scrollTrigger: {
+        start: "top 60%",
+        trigger: section,
+        toggleActions: "play none none reverse",
+      }
+    })
+
+    .from(title, 
+      {x: -50, opacity: 0})
+    .from(list,
+      {x: -50, opacity: 0}, '-= 0.5')
+    .from(image,
+      {y: 50, opacity: 0}, '-= 0.5')
+    .from(btn,
+      {y: 50, opacity: 0}, '-= 0.5')
+  });
